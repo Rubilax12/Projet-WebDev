206c021a08d17c0ec45bf0a9e0b673daf27fc741
MambaLite <145127257+Rubilax12@users.noreply.github.com>
2024-06-20 03:47:04 +0200

    On Avance
    Pas sur que ça sera fini pour 8h

---
+++ V1.51 CSS/pythonCoursCyber/app/__init__.py
@@ -0,0 +1,16 @@
+# Importer Flask pour la création de l'application
+from flask import Flask
+
+
+# Création de l'instance principale de l'application Flask
+app = Flask(__name__)
+
+
+
+
+# Chargement des configurations depuis le fichier config.py
+app.config.from_object('config')
+
+
+# Importation des vues depuis le dossier app
+from app import views
